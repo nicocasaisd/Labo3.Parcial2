4deaa239417b39ed98bbacfe452a4486e01d954e
nicocasaisd <nicolas.casais.dassie@gmail.com>
2023-07-14 16:08:01 -0300

    maxFuerza y minFuerza, funcionan

--- js/fuerza.js
+++ js/fuerza.js
@@ -1,10 +1,13 @@
 const $avgFuerza = document.getElementById("avgFuerza");
 const $maxFuerza = document.getElementById("maxFuerza");
+const $minFuerza = document.getElementById("minFuerza");
 
 export function AsignarPromedioFuerza(array) {
   // console.log('Promedio fuerza', array);
   $avgFuerza.value = calcularPromedioFuerza(array).toFixed(2);
+  // Maximos y Minimos
   $maxFuerza.value = calcularMaxFuerza(array);
+  $minFuerza.value = calcularMinFuerza(array);
 }
 
 // REDUCE
@@ -27,4 +30,21 @@ function calcularMaxFuerza(arr) {
   });
 
   return maximaFuerza;
+}
+
+// MAX
+function calcularMinFuerza(arr) {
+  let minimaFuerza;
+
+  let arrayFuerzas = arr.map((heroe) => heroe.fuerza);
+
+  minimaFuerza = arrayFuerzas.reduce((prev, actual) => {
+    if (prev <= actual) {
+      return prev;
+    } else {
+      return actual;
+    }
+  });
+
+  return minimaFuerza;
 }
